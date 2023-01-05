0a0dee740dbdbdc450017b83db109f218634c164
Greg <litm47@gmail.com>
2023-01-05 13:09:56 -0500

    started working on controllers

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
